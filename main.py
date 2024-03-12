e3e4e5723e3711c116cb21bc02c647a78569dbed
Asle-Oren <asleoren@gmail.com>
2024-03-12 18:53:17 +0100

    Second commit for mode switch. Ir only is not fully functional yet.

--- main.py
+++ main.py
@@ -54,35 +54,38 @@ def map_g_to_temp(g):
 
 
 while True:
+
+
     img = sensor.snapshot()
-    # ta: Ambient temperature
-    # ir: Object temperatures (IR array)
-    # to_min: Minimum object temperature
-    # to_max: Maximum object temperature
-    ta, ir, to_min, to_max = fir.read_ir()
-
-    fir.draw_ir(fir_img, ir, color_palette=None, hint=image.ROTATE_180)
-
-    fir_img_size = fir_img.width() * fir_img.height()
-
-    # Find IR Blobs
-    blobs = fir_img.find_blobs(threshold_list,
-                               pixels_threshold=(fir_img_size // 100),
-                               area_threshold=(fir_img_size // 100),
-                               merge=True)
-
-    # Collect stats into a list of tuples
-    blob_stats = []
-    for b in blobs:
-        blob_stats.append((b.rect(), map_g_to_temp(img.get_statistics(thresholds=threshold_list,
-                                                                      roi=b.rect()).mean())))
-    x_scale = img.width() / fir_img.width()
-    y_scale = img.height() / fir_img.height()
 
     if pin7.value() == 0: # switch down, show ir only
+        # ta: Ambient temperature
+        # ir: Object temperatures (IR array)
+        # to_min: Minimum object temperature
+        # to_max: Maximum object temperature
+        ta, ir, to_min, to_max = fir.read_ir()
+
+        fir.draw_ir(fir_img, ir, color_palette=image.PALETTE_RAINBOW, alpha_palette=alpha_pal, hint=image.ROTATE_180)
+
+        fir_img_size = fir_img.width() * fir_img.height()
+
+        # Find IR Blobs
+        blobs = fir_img.find_blobs(threshold_list,
+                                pixels_threshold=(fir_img_size // 100),
+                                area_threshold=(fir_img_size // 100),
+                                merge=True)
+
+        # Collect stats into a list of tuples
+        blob_stats = []
+        for b in blobs:
+            blob_stats.append((b.rect(), map_g_to_temp(img.get_statistics(thresholds=threshold_list,
+                                                                        roi=b.rect()).mean())))
+        x_scale = img.width() / fir_img.width()
+        y_scale = img.height() / fir_img.height()
+
         img.draw_image(fir_img, 0, 0, x_scale=x_scale, y_scale=y_scale,
                     color_palette=image.PALETTE_RAINBOW,
-                    alpha_palette=alpha_pal,
+                    alpha_palette=alpha_pal, alpha=256,
                     hint=image.BICUBIC)
 
         # Draw stuff on the colored image
@@ -98,11 +101,36 @@ while True:
         img.draw_string(40, 460, 'Max Temp: %0.2f C' % to_max,
                         color=(255, 255, 255), mono_space=False, scale=2, string_rotation=270)
 
+        lcd.write(img, hint=(image.BILINEAR | image.CENTER | image.SCALE_ASPECT_KEEP))
+
 
     elif pin8.value() == 0: #switch up, show camera only
-        pass
+        lcd.write(img, hint=(image.BILINEAR | image.CENTER | image.SCALE_ASPECT_KEEP))
 
     else: #switch neutral, show both camera and ir
+        # ta: Ambient temperature
+        # ir: Object temperatures (IR array)
+        # to_min: Minimum object temperature
+        # to_max: Maximum object temperature
+        ta, ir, to_min, to_max = fir.read_ir()
+
+        fir.draw_ir(fir_img, ir, color_palette=None, hint=image.ROTATE_180)
+
+        fir_img_size = fir_img.width() * fir_img.height()
+
+        # Find IR Blobs
+        blobs = fir_img.find_blobs(threshold_list,
+                                pixels_threshold=(fir_img_size // 100),
+                                area_threshold=(fir_img_size // 100),
+                                merge=True)
+
+        # Collect stats into a list of tuples
+        blob_stats = []
+        for b in blobs:
+            blob_stats.append((b.rect(), map_g_to_temp(img.get_statistics(thresholds=threshold_list,
+                                                                        roi=b.rect()).mean())))
+        x_scale = img.width() / fir_img.width()
+        y_scale = img.height() / fir_img.height()
 
         img.draw_image(fir_img, 0, 0, x_scale=x_scale, y_scale=y_scale,
                     color_palette=image.PALETTE_RAINBOW,
@@ -113,6 +141,12 @@ while True:
             img.draw_string(int((blob_stat[0][0] * x_scale) + 4), int((blob_stat[0][1] * y_scale) + 1),
                             '%.2f C' % blob_stat[1], mono_space=False, scale=2, string_rotation=270)
 
+        # Draw stuff on the colored image
+        for b in blobs:
+            img.draw_rectangle(int(b.rect()[0] * x_scale), int(b.rect()[1] * y_scale),
+                            int(b.rect()[2] * x_scale), int(b.rect()[3] * y_scale))
+            img.draw_cross(int(b.cx() * x_scale), int(b.cy() * y_scale))
+
         # Draw ambient, min and max temperatures.
         img.draw_string(0, 460, 'Lepton Temp: %0.2f C' % ta,
                         color=(255, 255, 255), mono_space=False, scale=2, string_rotation=270)
@@ -121,4 +155,4 @@ while True:
         img.draw_string(40, 460, 'Max Temp: %0.2f C' % to_max,
                         color=(255, 255, 255), mono_space=False, scale=2, string_rotation=270)
 
-    lcd.write(img, hint=(image.BILINEAR | image.CENTER | image.SCALE_ASPECT_KEEP))
+        lcd.write(img, hint=(image.BILINEAR | image.CENTER | image.SCALE_ASPECT_KEEP))
